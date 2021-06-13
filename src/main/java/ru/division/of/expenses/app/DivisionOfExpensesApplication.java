564c351f488c77d010c7a672138f94c1b3de6552
palandarus <klarkken@yandex.ru>
2021-06-13 18:53:36 +0300

    Update DivisionOfExpensesApplication.java

--- src/main/java/ru/division/of/expenses/app/DivisionOfExpensesApplication.java
+++ src/main/java/ru/division/of/expenses/app/DivisionOfExpensesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:secured.properties")
 public class DivisionOfExpensesApplication {
 
 	public static void main(String[] args) {
